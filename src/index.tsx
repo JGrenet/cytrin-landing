0616bb75b8997687fe1fa728a1d8d0257c939daf
JGrenet <contact@jeremygrenet.com>
2021-07-09 17:52:56 +0200

    chore: basic project booting

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style/index.scss";
+
+ReactDOM.render(
+  <div className="root" />,
+  document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+ 
+module.exports = {
+    //path to entry paint
+    entry: './src/index.tsx',
+ 
+    //path and filename of the final output
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    resolve: {
+		extensions: ['.ts', '.tsx', '.js']
+	},
+
+    module: {
+		rules: [
+			{ test: /\.(tsx|ts)?$/, exclude: /node_modules/, use: "babel-loader" },
+			{ test: /\.scss?$/, exclude: /node_modules/, use: ["style-loader", "css-loader", "sass-loader"] }
+		]
+	},
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+        }),
+        new webpack.ProvidePlugin({
+            React: "react",
+            ReactDOM: "react-dom",
+        }),
+    ],
+ 
+    //default mode is production
+    mode: 'development',
+
+    devtool: "source-map",
+
+    devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+        historyApiFallback: true,
+		compress: true
+	}
+}
